7e01bc51472f716a6b9f28a21a8799ebeb07e1bc
Ewan Pandelus <ewanpandelus@hotmail.com>
2021-12-26 14:46:29 +0000

    Initial code base

---
+++ Tutorial 7/Engine/Camera.h
@@ -0,0 +1,34 @@
+#pragma once
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+
+	void							Update();
+	DirectX::SimpleMath::Matrix		getCameraMatrix();
+	void							setPosition(DirectX::SimpleMath::Vector3 newPosition);
+	DirectX::SimpleMath::Vector3	getPosition();
+	DirectX::SimpleMath::Vector3	getForward();
+	void							setRotation(DirectX::SimpleMath::Vector3 newRotation);
+	DirectX::SimpleMath::Vector3	getRotation();
+	float							getMoveSpeed();
+	float							getRotationSpeed();
+	void							SetIsCamera2(bool isCamera2);
+
+private:
+	DirectX::SimpleMath::Matrix		m_cameraMatrix;			//camera matrix to be passed out and used to set camera position and angle for wrestling
+	DirectX::SimpleMath::Vector3	m_lookat;
+	DirectX::SimpleMath::Vector3	m_position;
+	DirectX::SimpleMath::Vector3	m_forward;
+	DirectX::SimpleMath::Vector3	m_right;
+	DirectX::SimpleMath::Vector3	m_up;
+	DirectX::SimpleMath::Vector3	m_orientation;			//vector storing pitch yaw and roll. 
+
+	bool isCamera2 = false;
+
+	float	m_movespeed ;	
+	float	m_camRotRate;
+
+};
+
---
+++ Tutorial 7/Engine/Game.h
@@ -0,0 +1,144 @@
+//
+// Game.h
+//
+#pragma once
+#include "SkyboxEffect.h"
+#include "DeviceResources.h"
+#include "StepTimer.h"
+#include "Shader.h"
+#include "modelclass.h"
+#include "Light.h"
+#include "Input.h"
+#include "Camera.h"
+#include "RenderTexture.h"
+
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop.
+class Game final : public DX::IDeviceNotify
+{
+public:
+
+    Game() noexcept(false);
+    ~Game();
+
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+
+    // IDeviceNotify
+    virtual void OnDeviceLost() override;
+    virtual void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowMoved();
+    void OnWindowSizeChanged(int width, int height);
+#ifdef DXTK_AUDIO
+    void NewAudioDevice();
+#endif
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const;
+	
+private:
+
+	struct MatrixBufferType
+	{
+		DirectX::XMMATRIX world;
+		DirectX::XMMATRIX view;
+		DirectX::XMMATRIX projection;
+	}; 
+
+    void Update(DX::StepTimer const& timer);
+    void Render();
+	void RenderTexturePass1();
+    void Clear();
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+    void AddFog(ID3D11Device* device, const wchar_t* szFileName);
+    float xChange;
+    float yChange;
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                           m_timer;
+
+	//input manager. 
+	Input									m_input;
+	InputCommands							m_gameInputCommands;
+
+    // DirectXTK objects.
+    std::unique_ptr<DirectX::CommonStates>                                  m_states;
+    std::unique_ptr<DirectX::BasicEffect>                                   m_batchEffect;	
+    std::unique_ptr<DirectX::EffectFactory>                                 m_fxFactory;
+    std::unique_ptr<DirectX::SpriteBatch>                                   m_sprites;
+    std::unique_ptr<DirectX::SpriteFont>                                    m_font;
+
+	// Scene Objects
+	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>>  m_batch;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout>                               m_batchInputLayout;
+	std::unique_ptr<DirectX::GeometricPrimitive>                            m_testmodel;
+
+	//lights
+	Light																	m_Light;
+
+	//Cameras
+	Camera																	m_Camera01;
+    Camera                                                                  m_Camera02;
+
+	//textures 
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>                        m_texture1;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>                        m_texture2;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>                        m_texture3;
+
+	//Shaders
+	Shader																	m_BasicShaderPair;
+	ModelClass																m_BasicModel;
+	ModelClass																m_BasicModel2;
+	ModelClass																m_BasicModel3;
+    ModelClass																m_BasicModel4;
+    ModelClass																m_BasicModel5;
+
+	//RenderTextures
+	RenderTexture*															m_FirstRenderPass;
+	RECT																	m_fullscreenRect;
+	RECT																	m_CameraViewRect;
+	
+
+
+#ifdef DXTK_AUDIO
+    std::unique_ptr<DirectX::AudioEngine>                                   m_audEngine;
+    std::unique_ptr<DirectX::WaveBank>                                      m_waveBank;
+    std::unique_ptr<DirectX::SoundEffect>                                   m_soundEffect;
+    std::unique_ptr<DirectX::SoundEffectInstance>                           m_effect1;
+    std::unique_ptr<DirectX::SoundEffectInstance>                           m_effect2;
+#endif
+    
+
+#ifdef DXTK_AUDIO
+    uint32_t                                                                m_audioEvent;
+    float                                                                   m_audioTimerAcc;
+
+    bool                                                                    m_retryDefault;
+#endif
+
+    DirectX::SimpleMath::Matrix                                             m_world;
+    DirectX::SimpleMath::Matrix                                             m_view;
+    DirectX::SimpleMath::Matrix                                             m_view2;
+    DirectX::SimpleMath::Matrix                                             m_projection;
+    std::unique_ptr<DirectX::GeometricPrimitive> m_sky;
+    std::unique_ptr<DX::SkyboxEffect> m_effect;
+
+    Microsoft::WRL::ComPtr<ID3D11InputLayout> m_skyInputLayout;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_cubemap;
+
+    std::unique_ptr<DirectX::IEffectFactory> m_fxFactory2;
+    std::unique_ptr<DirectX::Model> m_model;
+};
